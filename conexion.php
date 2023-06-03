de6f3ca203e1535e5ec883f60f4c32969ef476c1
luiseduardogp <92336594+luiseduardogp@users.noreply.github.com>
2023-06-03 01:36:03 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:devopsdatabaseunimag.database.windows.net,1433; Database = devopsdatabase", "devopsdatabaseunimag", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:devopsdatabaseunimag.database.windows.net,1433; Database = devopsdatabase", "devopsdatabaseunimag", "{Luis99080704247}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
